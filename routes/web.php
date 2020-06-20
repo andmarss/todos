b9ecf5a0549e06a0cb0a473e1feb46bbe052adf7
Андрей Марков <anrey-markov@mail.ru>
2020-06-20 18:11:10 +0300

    add new routes

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,20 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use \Illuminate\Support\Facades\{Auth, Route};
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'auth', 'prefix' => 'todo', 'as' => 'todo.'], function () {
+    Route::get('/', 'TodoController@index')->name('index');
+
+    Route::post('/create', 'TodoController@store')->name('create');
+
+    Route::post('/update/{id}', 'TodoController@update')->name('update');
+    Route::post('/delete/{id}', 'TodoController@destroy')->name('delete');
+});
+
+Route::group(['middleware' => 'guest'], function () {
+    Auth::routes();
+
+    Route::get('/', function (){
+        return redirect()->route('login');
+    })->name('index');
 });
